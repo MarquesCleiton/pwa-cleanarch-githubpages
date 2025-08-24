88ac40244a86e7c8217c50f6390a97722b16565d
marquescleiton <cleiton.contato2210@gmail.com>
2025-08-23 22:49:05 -0300

    feat: projeto PWA base com Clean Arch e deploy via GitHub Pages

---
+++ src/presentation/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const HomePage = () => {
+  return (
+    <main>
+      <h1>Hello PWA on GitHub Pages!</h1>
+      <p>Clean Arch | TypeScript | React | Vite</p>
+    </main>
+  );
+};
