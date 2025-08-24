71c95ad8347b9fe32c89b62e672cfb89fcf8495e
marquescleiton <cleiton.contato2210@gmail.com>
2025-08-23 23:29:18 -0300

    novo commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   base: '/pwa-cleanarch-githubpages/',
-  plugins: [react()]
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/service-worker.js',
+          dest: '' // copia para raiz do dist
+        }
+      ]
+    })
+  ]
 });
